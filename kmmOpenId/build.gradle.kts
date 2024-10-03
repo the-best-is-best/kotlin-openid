4a32f7ee065cf8225a93fa61c9f6995ad759b520
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-10-03 23:15:37 +0300

    Use kotlin 2.0.21-RC for support xcode 16

--- kmmOpenId/build.gradle.kts
+++ kmmOpenId/build.gradle.kts
@@ -41,7 +41,7 @@ tasks.withType<PublishToMavenRepository> {
 
 
 mavenPublishing {
-    coordinates("io.github.the-best-is-best", "kapp-auth", "1.0.0")
+    coordinates("io.github.the-best-is-best", "kapp-auth", "1.0.1")
 
     publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
     signAllPublications()
