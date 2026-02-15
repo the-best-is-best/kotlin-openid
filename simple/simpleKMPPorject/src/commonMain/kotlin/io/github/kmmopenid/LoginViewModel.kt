c25619e39be770839b36b85108e0c5a157216596
Michelle Raouf <michelle.raouf@52ndsolution.net>
2026-02-15 23:31:55 +0200

    Refactor: Remove unused `AuthEvent` and `AuthState` sealed interfaces
    This commit removes the `AuthEvent` and `AuthState` sealed interfaces from `LoginViewModel.kt` in the `commonMain` source set. These interfaces were unused and have been removed to clean up the codebase.

--- simple/simpleKMPPorject/src/commonMain/kotlin/io/github/kmmopenid/LoginViewModel.kt
+++ simple/simpleKMPPorject/src/commonMain/kotlin/io/github/kmmopenid/LoginViewModel.kt
@@ -4,17 +4,6 @@ import androidx.lifecycle.ViewModel
 import io.github.openid.AuthResult
 import kotlinx.coroutines.flow.StateFlow
 
-sealed interface AuthEvent {
-    object StartOpenIdLogin : AuthEvent
-    object StartOpenIdLogout : AuthEvent
-}
-
-sealed interface AuthState {
-    data class LoginSuccess(val authResult: AuthResult) : AuthState
-    data class LoginFailed(val message: String) : AuthState
-    object LogoutSuccess : AuthState
-    data class LogoutFailed(val message: String) : AuthState
-}
 
 expect class LoginViewModel : ViewModel {
     val userInfo: StateFlow<AuthResult?>
